0b1f7ed5dc69de9c1ef7fc04fdf86e920bc03301
Denisa <falcusandenisa75@gmail.com>
2026-02-18 19:18:22 +0200

    sign-in

--- mobile/app/(auth)/sign-in.jsx
+++ mobile/app/(auth)/sign-in.jsx
@@ -1,9 +1,18 @@
 import { useSignIn } from "@clerk/clerk-expo";
-// import type { EmailCodeFactor } from '@clerk/types'
 import { Link, useRouter } from "expo-router";
 import { useCallback, useState } from "react";
-import { Pressable, TextInput, View, Text } from "react-native";
+import {
+  Pressable,
+  TextInput,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { styles } from "@/assets/styles/auth.styles.js";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Ionicons } from "@expo/vector-icons";
+import { COLORS } from "../../constants/colors";
 
 export default function Page() {
   const { signIn, setActive, isLoaded } = useSignIn();
@@ -13,59 +22,52 @@ export default function Page() {
   const [password, setPassword] = useState("");
   const [code, setCode] = useState("");
   const [showEmailCode, setShowEmailCode] = useState(false);
+  const [error, setError] = useState("");
 
-  // Handle the submission of the sign-in form
   const onSignInPress = useCallback(async () => {
-    if (!isLoaded) return;
-
-    // Start the sign-in process using the email and password provided
-    try {
-      const signInAttempt = await signIn.create({
-        identifier: emailAddress,
-        password,
-      });
-
-      // If sign-in process is complete, set the created session as active
-      // and redirect the user
-      if (signInAttempt.status === "complete") {
-        await setActive({
-          session: signInAttempt.createdSessionId,
-          navigate: async ({ session }) => {
-            if (session?.currentTask) {
-              // Check for tasks and navigate to custom UI to help users resolve them
-              console.log(session?.currentTask);
-              return;
-            }
-
-            router.replace("/");
-          },
+    if (!isLoaded)
+      try {
+        const signInAttempt = await signIn.create({
+          identifier: emailAddress,
+          password,
         });
-      } else if (signInAttempt.status === "needs_second_factor") {
-        // Check if email_code is a valid second factor
-        // This is required when Client Trust is enabled and the user
-        // is signing in from a new device.
-        const emailCodeFactor = signInAttempt.supportedSecondFactors?.find(
-          (factor) => factor.strategy === "email_code",
-        );
 
-        if (emailCodeFactor) {
-          await signIn.prepareSecondFactor({
-            strategy: "email_code",
-            emailAddressId: emailCodeFactor.emailAddressId,
+        if (signInAttempt.status === "complete") {
+          await setActive({
+            session: signInAttempt.createdSessionId,
+            navigate: async ({ session }) => {
+              if (session?.currentTask) {
+                console.log(session?.currentTask);
+                return;
+              }
+
+              router.replace("/");
+            },
           });
-          setShowEmailCode(true);
+        } else if (signInAttempt.status === "needs_second_factor") {
+          const emailCodeFactor = signInAttempt.supportedSecondFactors?.find(
+            (factor) => factor.strategy === "email_code",
+          );
+
+          if (emailCodeFactor) {
+            await signIn.prepareSecondFactor({
+              strategy: "email_code",
+              emailAddressId: emailCodeFactor.emailAddressId,
+            });
+            setShowEmailCode(true);
+          }
+        } else {
+          console.error(JSON.stringify(signInAttempt, null, 2));
         }
-      } else {
-        // If the status is not complete, check why. User may need to
-        // complete further steps.
-        console.error(JSON.stringify(signInAttempt, null, 2));
+      } catch (err) {
+        if (err.errors?.[0]?.code === "form_password_incorrect") {
+          setError("Password is incorrect. Please try again.");
+        } else {
+          setError("An error occurred. Please try again.");
+        }
       }
-    } catch (err) {
-      console.error(JSON.stringify(err, null, 2));
-    }
   }, [isLoaded, signIn, setActive, router, emailAddress, password]);
 
-  // Handle the submission of the email verification code
   const onVerifyPress = useCallback(async () => {
     if (!isLoaded) return;
 
@@ -80,7 +82,6 @@ export default function Page() {
           session: signInAttempt.createdSessionId,
           navigate: async ({ session }) => {
             if (session?.currentTask) {
-              // Check for tasks and navigate to custom UI to help users resolve them
               console.log(session?.currentTask);
               return;
             }
@@ -95,8 +96,6 @@ export default function Page() {
       console.error(JSON.stringify(err, null, 2));
     }
   }, [isLoaded, signIn, setActive, router, code]);
-
-  // Display email code verification form
   if (showEmailCode) {
     return (
       <View style={styles.container}>
@@ -128,98 +127,78 @@ export default function Page() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text type="title" style={styles.title}>
-        Sign in
-      </Text>
-      <Text style={styles.label}>Email address</Text>
-      <TextInput
-        style={styles.input}
-        autoCapitalize="none"
-        value={emailAddress}
-        placeholder="Enter email"
-        placeholderTextColor="#666666"
-        onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-        keyboardType="email-address"
-      />
-      <Text style={styles.label}>Password</Text>
-      <TextInput
-        style={styles.input}
-        value={password}
-        placeholder="Enter password"
-        placeholderTextColor="#666666"
-        secureTextEntry={true}
-        onChangeText={(password) => setPassword(password)}
-      />
-      <Pressable
-        style={({ pressed }) => [
-          styles.button,
-          (!emailAddress || !password) && styles.buttonDisabled,
-          pressed && styles.buttonPressed,
-        ]}
-        onPress={onSignInPress}
-        disabled={!emailAddress || !password}
-      >
-        <Text style={styles.buttonText}>Sign in</Text>
-      </Pressable>
-      <View style={styles.linkContainer}>
-        <Text>Don&apos;t have an account?</Text>
-        <Link href="/sign-up">
-          <Text type="link">Sign up</Text>
-        </Link>
+    <KeyboardAwareScrollView
+      style={{ flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1 }}
+      enableOnAndroid={true}
+      enableAutomaticScroll={true}
+      extraScrollHeight={100}
+    >
+      <View>
+        <View style={styles.container}>
+          <Image
+            source={require("../../assets/images/revenue-i4.png")}
+            style={styles.illustration}
+          />
+          <Text type="title" style={styles.title}>
+            Welcome Back
+          </Text>
+          {error ? (
+            <View style={styles.errorBox}>
+              <Ionicons name="alert-circle" size={20} color={COLORS.expense} />
+              <Text style={styles.errorText}>{error}</Text>
+              <TouchableOpacity on Prress={() => setError("")}>
+                <Ionicons name="close" size={20} color={COLORS.textLight} />
+              </TouchableOpacity>
+            </View>
+          ) : null}
+
+          <Text style={styles.label}>Email address</Text>
+          <TextInput
+            style={[styles.input, error && styles.errorInput]}
+            autoCapitalize="none"
+            value={emailAddress}
+            placeholder="Enter email"
+            placeholderTextColor="#9a8478"
+            onChangeText={(email) => setEmailAddress(email)}
+            keyboardType="email-address"
+          />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={[styles.input, error && styles.errorInput]}
+            value={password}
+            placeholder="Enter password"
+            placeholderTextColor="#9A8478"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+
+          {/* <Pressable
+            style={({ pressed }) => [
+              styles.button,
+              (!emailAddress || !password) && styles.buttonDisabled,
+              pressed && styles.buttonPressed,
+            ]}
+            onPress={onSignInPress}
+            disabled={!emailAddress || !password}
+          >
+            <Text style={styles.buttonText}>Sign in</Text>
+          </Pressable> */}
+
+          <TouchableOpacity style={styles.button} onPress={onSignInPress}>
+            <Text style={styles.buttonText}>Sign in</Text>
+          </TouchableOpacity>
+
+          <View style={styles.footerContainer}>
+            <Text style={styles.footerText}>Don&apos;t have an account?</Text>
+            <Link href="/sign-up" asChild>
+              <TouchableOpacity>
+                <Text style={styles.linkText}>Sign up</Text>
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
       </View>
-    </View>
+    </KeyboardAwareScrollView>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//     gap: 12,
-//   },
-//   title: {
-//     marginBottom: 8,
-//   },
-//   description: {
-//     fontSize: 14,
-//     marginBottom: 16,
-//     opacity: 0.8,
-//   },
-//   label: {
-//     fontWeight: "600",
-//     fontSize: 14,
-//   },
-//   input: {
-//     borderWidth: 1,
-//     borderColor: "#ccc",
-//     borderRadius: 8,
-//     padding: 12,
-//     fontSize: 16,
-//     backgroundColor: "#fff",
-//   },
-//   button: {
-//     backgroundColor: "#0a7ea4",
-//     paddingVertical: 12,
-//     paddingHorizontal: 24,
-//     borderRadius: 8,
-//     alignItems: "center",
-//     marginTop: 8,
-//   },
-//   buttonPressed: {
-//     opacity: 0.7,
-//   },
-//   buttonDisabled: {
-//     opacity: 0.5,
-//   },
-//   buttonText: {
-//     color: "#fff",
-//     fontWeight: "600",
-//   },
-//   linkContainer: {
-//     flexDirection: "row",
-//     gap: 4,
-//     marginTop: 12,
-//     alignItems: "center",
-//   },
-// });
