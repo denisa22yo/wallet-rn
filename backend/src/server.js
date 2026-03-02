3ab1b1f863a53b6f2c0e997a465ff91b7dbb4fcd
Denisa <falcusandenisa75@gmail.com>
2026-03-02 20:23:04 +0200

    backend + ui/ux changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,25 @@
-import express from "express";
+import express, { request } from "express";
 import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 dotenv.config();
 
 const app = express();
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
 app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
+
+app.get("/api/health", (req, res) => {
+  (res.status(200), json({ status: "ok" }));
+});
 app.use("/api/transactions", transactionsRoute);
 
 // app.get("/api/health-check", (req, res) => {
--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,52 +1,116 @@
-import { SignOutButton } from "@/components/SignOutButton";
-import { SignedIn, SignedOut, useSession, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { useSession, useUser } from "@clerk/clerk-expo";
+import {
+  Alert,
+  FlatList,
+  Image,
+  RefreshControl,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useTransactions } from "../../hooks/useTransactions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useRouter } from "expo-router";
+import PageLoader from "../../components/PageLoader";
+import { styles } from "../../assets/styles/home.styles";
+import { Ionicons } from "@expo/vector-icons";
+import { SignOutButton } from "@/components/SignOutButton";
+import { TransactionItem } from "@/components/TransactionItem";
+import { BalanceCard } from "../../components/BalanceCard";
+import NoTransactionsFound from "../../components/NoTransactionsFound";
 
 export default function Page() {
   const { user } = useUser();
+  const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
+
   const { transactions, summary, isLoading, loadData, deleteTransaction } =
     useTransactions(user.id);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await loadData();
+    setRefreshing(false);
+  };
 
   useEffect(() => {
     loadData();
   }, [loadData]);
 
-  console.log("transactions", transactions);
-  console.log("summary", summary);
+  const handleDelete = (id) => {
+    Alert.alert(
+      "Delete Transaction",
+      "Are you sure you want to delete this transaction?",
+      [
+        { text: "Cancel", style: "cancel" },
+        {
+          text: "Delete",
+          style: "destructive",
+          onPress: () => deleteTransaction(id),
+        },
+      ],
+    );
+  };
 
   // If your user isn't appearing as signed in,
   // it's possible they have session tasks to complete.
   const { session } = useSession();
   console.log(session?.currentTask);
 
+  if (isLoading && !refreshing) return <PageLoader />;
+
   return (
     <View style={styles.container}>
-      <Text>Welcome!</Text>
-      {/* Show the sign-in and sign-up buttons when the user is signed out */}
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
-      {/* Show the sign-out button when the user is signed in */}
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
+      <View style={styles.content}>
+        {/* HEADER */}
+        <View style={styles.header}>
+          {/* LEFT (icon, welcome, name) */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {/* falcusandenisa75@gmail.com -> va aparea ce este inainte de @*/}
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          {/* RIGHT ( 2x buttons) */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#fff" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      <FlatList
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        renderItem={({ item }) => (
+          <TransactionItem item={item} onDelete={handleDelete} />
+        )}
+        ListEmptyComponent={<NoTransactionsFound />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    gap: 16,
-  },
-});
